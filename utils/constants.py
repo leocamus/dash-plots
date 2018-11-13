fe65f31bebe624e70b7863332be8d15f9e19434e
LeoCamus <leocamusc@gmail.com>
2018-11-13 18:16:01 -0300

    this is a test

--- utils/constants.py
+++ utils/constants.py
@@ -6,3 +6,4 @@ travel_times_dir = os.path.join(five_levels_up, '02_datasets/02_travel_times')
 
 waze_travel_times_dir = os.path.join(travel_times_dir, 'waze')
 gps_travel_times_dir = os.path.join(travel_times_dir, 'gps')
+#this is a test
